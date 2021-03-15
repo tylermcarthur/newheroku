dabeb8324e9a2ac11a8a5c95592704ae585430c0
tylermcarthur <tylermcarthur@yahoo.com>
2021-03-15 15:51:14 -0500

    boop

--- server.js
+++ server.js
@@ -2,16 +2,20 @@ require('dotenv').config()
 const express = require('express');
 const pool = require('./db/db_configuration.js');
 const app = express();
-const db = require('./db/db_configuration.js');
 
 app.use(express.static('public'))
 
 app.get('/api/students', async(req, res) => {
-    const client = await pool.connect
-    pool.query("SELECT * FROM student", (err, data) => {
-        res.json(data.rows);
-        client.release();
-    })
+    try {
+        const client = await pool.connect
+        const students = pool.query("SELECT * FROM student", (err, data) => {
+            res.json(data.rows);
+            client.release();
+        })
+    }
+        catch (error) {
+        res.end('boop')
+    }
 })
 
 
