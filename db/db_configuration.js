d1f2795d2a7074d9354fb929cd6a80c07999108e
tylermcarthur <tylermcarthur@yahoo.com>
2021-03-15 15:36:02 -0500

    shouldbedone

--- db/db_configuration.js
+++ db/db_configuration.js
@@ -1,11 +1,8 @@
-const { Pool } = require('pg')
-
+const { Pool } = require('pg');
 const pool = new Pool({
-    user: process.env.DATABASE_USER,
-    database: process.env.DATABASE_NAME,
-    password: process.env.DATABASE_PASSWORD,
-    host:"localhost",
-    port: 5432
-})  
-
+  connectionString: process.env.DATABASE_URL,
+  ssl: {
+    rejectUnauthorized: false
+  }
+});
 module.exports = pool
--- server.js
+++ server.js
@@ -1,13 +1,16 @@
 require('dotenv').config()
 const express = require('express');
+const pool = require('./db/db_configuration.js');
 const app = express();
 const db = require('./db/db_configuration.js');
 
 app.use(express.static('public'))
 
-app.get('/', (req, res) => {
-    db.query("SELECT * FROM student;", (err, data) => {
+app.get('/api/students', (req, res) => {
+    const client = await pool.connect
+    client.query("SELECT * FROM student;", (err, data) => {
         res.json(data.rows);
+        client.release();
     })
 })
 
